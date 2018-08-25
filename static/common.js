54a49ae7fefe2946f365d6b1ce2b432d8315710e
Benjamin S <benni.s@slideup.de>
2018-08-25 17:31:58 +0200

    Debug-Webseite - Verbesserungen
    Signed-off-by: Benjamin S <benni.s@slideup.de>

--- static/common.js
+++ static/common.js
@@ -17,4 +17,22 @@ $(document).ready(function () {
         kik.showProfile("#"+$(event.target).data("group-id"));
         return false;
     });
+
+    $(document).on("keydown", "#message_body", function (event) {
+        if ((event.keyCode === 10 || event.keyCode === 13) && event.ctrlKey) {
+            $("#message_body").closest("form").submit()
+        }
+    });
+
+    $(document).on("click", ".keyboard", function (event) {
+        $('#message_body').val($(event.target).html());
+        if (event.ctrlKey) {
+            $("#message_body").closest("form").submit()
+        }
+    });
+    $(document).on("submit", "form", function (event) {
+        $(event.target).find("input[type=submit]").attr("disabled", "disabled");
+        $(event.target).unbind('submit');
+    });
+    $('#message_body').select();
 });
